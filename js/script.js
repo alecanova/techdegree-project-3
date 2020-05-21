42f75e893456e7fb4fdac84379ef82f955216d4b
Alessandro Canova <alessandrocanova14@gmail.com>
2020-05-21 23:55:19 +0200

    colorOption and defaultOption()

--- js/script.js
+++ js/script.js
@@ -6,6 +6,7 @@ const jobRole = document.getElementById('title');
 const otherJobInput = document.getElementById('other-title');
 const otherJobLabel = document.querySelectorAll('label')[3];
 
+// Other job role
 otherJobInput.classList.add('is-hidden');
 otherJobLabel.classList.add('is-hidden');
 
@@ -19,3 +20,29 @@ jobRole.addEventListener ('change', (e) => {
     }
 });
 
+// T-shirts
+const colorOptions = document.getElementById('color');
+const designOptions = document.getElementById('design');
+
+colorOptions.classList.add('is-hidden');
+designOptions.options[0].classList.add('is-hidden');
+const pleaseSelect = document.createElement('option');
+pleaseSelect.value = 'please';
+pleaseSelect.innerHTML = 'Please select a T-shirt theme';
+colorOptions.setAttribute('selected', true);
+colorOptions.setAttribute('disabled', true);
+colorOptions.insertBefore(pleaseSelect, colorOptions.options[0]);
+
+
+const defaultOption = () => {
+    for (let i = 0; i < colorOptions.options.length; i++){
+        const please = RegExp(/Please/g);
+// innerText will not return the text of elements that are hidden with CSS (textContent will). 
+        if (!please.test(colorOptions.options[i].innerText)) {
+           colorOptions.options[i].classList.add('is-hidden')
+        } else {
+            colorOptions.selectedIndex = i
+        }
+    }
+}
+defaultOption()
