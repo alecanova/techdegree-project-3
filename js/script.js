6ccc4e634d6013bfc56a3557ef4c77bda70f9f40
Alessandro Canova <alessandrocanova14@gmail.com>
2020-05-28 00:09:33 +0200

    Added comments

--- js/script.js
+++ js/script.js
@@ -6,6 +6,7 @@ document.getElementById('name').focus();
               * OTHER JOB ROLE *
 ************************************************/
 
+// Selecting and hiding the elements just created in the HTML file.
 const jobRole = document.getElementById('title');
 const otherJobInput = document.getElementById('other-title');
 const otherJobLabel = document.querySelectorAll('label')[3];
@@ -13,6 +14,7 @@ const otherJobLabel = document.querySelectorAll('label')[3];
 otherJobInput.hidden = true;
 otherJobLabel.hidden = true;
 
+//if the user selects 'other', the other job input will appear.
 jobRole.addEventListener ('change', (e) => {
     
     if ( e.target.value === 'other') {
@@ -33,21 +35,25 @@ jobRole.addEventListener ('change', (e) => {
               * T-SHIRT SECTION *
 ************************************************/
 
+// Selecting the T-shirt info elements.
 const colorDiv = document.getElementById("colors-js-puns");
 const colorSelect = document.getElementById('color');
 const designSelect = document.getElementById('design');
 const colorOptions = document.getElementById('color').options;
 const designOptions = document.getElementById('design').options;
 designOptions[0].hidden = true;
+// Creating a new option in the color menu
 const pleaseSelect = document.createElement('option');
 pleaseSelect.value = '';
 pleaseSelect.innerHTML = 'Please select a T-shirt theme';
 colorSelect.insertBefore(pleaseSelect, colorOptions[0]);
 colorOptions[0].selected = true;
-//colorDiv.hidden = true; ==> exceeds
+// Initially hiding the color menu 
+colorDiv.hidden = true; 
 
+// When one of the two themes is selected, only the appropriate colors will appear in the menu.
 designSelect.addEventListener('change', (e) => {
-    //colorDiv.hidden = false; ==> exceeds
+    colorDiv.hidden = false; 
     
     for(let i = 0; i < designOptions.length; i++) {
 
@@ -83,13 +89,14 @@ designSelect.addEventListener('change', (e) => {
            * REGISTER FOR ACTIVITIES *
 ************************************************/
 
+// Creating an element to display the total activity cost.
 const activities = document.getElementsByClassName('activities');
 const activitiesInput = document.querySelectorAll('.activities input'); 
 const totalCostDiv = document.createElement('div');
 activities[0].appendChild(totalCostDiv);
 let totalCost = 0;
 
-
+// Updating and displaying the total activity cost.
 activities[0].addEventListener('change', (e) => {
     const clicked = e.target;
 
@@ -106,7 +113,8 @@ activities[0].addEventListener('change', (e) => {
     }
 
     totalCostDiv.textContent = `Total: $ ${totalCost}`;
-
+    
+    // Disabling conflicting activities.
     for(let i = 0; i < activitiesInput.length; i++) {
 
         clickedDayAndTime = clicked.getAttribute('data-day-and-time');
@@ -136,6 +144,7 @@ activities[0].addEventListener('change', (e) => {
                 * PAYMENT SECTION *
 ************************************************/
 
+// Selecting and hiding payment options ( credit card will be the default option).
 const paymentSelect = document.getElementById('payment');
 const creditCardDiv = document.getElementById('credit-card');
 const paypalDiv = document.getElementById('paypal');
@@ -176,6 +185,7 @@ paymentSelect.addEventListener('change', (e) => {
 
 /***Error message function***/
 
+// This function assigns an error message to every input only if is invalid.
 const showErrorMessage = (divMsgId, errorMsg, target) => {
 
     const divMsg = document.createElement('div');
@@ -190,7 +200,8 @@ const showErrorMessage = (divMsgId, errorMsg, target) => {
 
 const nameInput = document.getElementById('name');
 const nameValidation = () => {
-
+    
+    // This function uses one RegExp to test the name input.
     const nameTest = /^([a-zA-Z ]){2,30}$/.test(nameInput.value);
     
     if (nameTest === true) {
@@ -217,6 +228,7 @@ nameInput.addEventListener('input', nameValidation);
 const emailInput = document.getElementById('mail');
 const emailValidation = () => {
 
+    // This function uses one RegExp to test the email input.
     const emailTest = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/.test(emailInput.value);
 
     if (emailTest === true) {
@@ -239,6 +251,7 @@ emailInput.addEventListener('input', emailValidation);
 
 /***Activities validation***/
 
+// If the user does not select any activities, an error message will appear.
 const activityValidation = () => {
    for(let i = 0; i < activitiesInput.length; i++) {
 
@@ -265,6 +278,8 @@ const creditCardInput = document.getElementById('cc-num')
 
 const creditCardValidation = () => {
 
+    /* This function uses three RegExp to test the credit card input
+    with two conditional error messages*/
     const ccTest1 = /^\d{13,16}$/.test(creditCardInput.value);
     const ccTest2 = /^\d{1,12}$/.test(creditCardInput.value);
     const ccTest3 = /^\d{17,}$/.test(creditCardInput.value);
@@ -301,6 +316,8 @@ const zipCodeInput = document.getElementById('zip');
 
 const zipCodeValidation = () => {
 
+    /* This function uses three RegExp to test the zip code input
+    with two conditional error messages*/
     const zipCodeTest1 = /^\d{5}$/.test(zipCodeInput.value);
     const zipCodeTest2 = /^\d{1,4}$/.test(zipCodeInput.value);
     const zipCodeTest3 = /^\d{6,}$/.test(zipCodeInput.value);
@@ -336,6 +353,8 @@ const cvvInput = document.getElementById('cvv');
 
 const cvvValidation = () => {
 
+    /* This function uses three RegExp to test the cvv input
+    with two conditional error messages*/
     const cvvTest1 = /^\d{3}$/.test(cvvInput.value);
     const cvvTest2 = /^\d{1,2}$/.test(cvvInput.value);
     const cvvTest3 = /^\d{4,}$/.test(cvvInput.value);
@@ -364,7 +383,43 @@ const cvvValidation = () => {
 cvvValidation();
 cvvInput.addEventListener('input', cvvValidation);
 
-//***submit event listener***//lement could prevent the default submission behavior of the form
+//***Submit event listener***//
+
+// Preventing the default submission behavior of the form.
+
+const form = document.querySelector('form');
+
+form.addEventListener('submit', (e) => {
+
+    if ( !nameValidation() ) {
+        e.preventDefault();
+    }
+
+    if ( !emailValidation() ) {
+        e.preventDefault();
+    }
+
+    if ( !activityValidation() ) {
+        e.preventDefault();
+    }
+
+    if ( paymentSelect[1].selected) {
+
+        if ( !creditCardValidation() ) {
+            e.preventDefault();
+        }
+
+        if ( !zipCodeValidation() ) {
+            e.preventDefault();
+        }
+
+        if ( !cvvValidation() ) {
+            e.preventDefault();
+        }
+
+    }
+    
+});
 
 
 
